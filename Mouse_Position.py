9054724cfd358242ac3bf9a46c49281601e93e3e
Afsalnitk <127192860+Afsalnitk@users.noreply.github.com>
2023-11-08 19:08:24 +0530

    Delete Mouse_Position.py

--- Mouse_Position.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-import time
-
-time.sleep(3)
-# Get mouse position
-currentMouseX, currentMouseY = pyautogui.position()
-print(f"Current mouse position: X={currentMouseX}, Y={currentMouseY}")
